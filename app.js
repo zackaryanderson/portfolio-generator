74e411d00706ce40e48b62556b7fccf523e2b9fe
zanderson1998 <zanderson1998@yahoo.com>
2021-01-11 15:02:13 -0700

    add basic arrow functions to print array

---
+++ app.js
@@ -0,0 +1,15 @@
+const profileDataArgs = process.argv.slice(2,process.argv.length);
+// console.log(profileDataArgs);
+
+const printProfileData = (profileDataArr) => {
+    //this ...
+    for ( let i = 0; i < profileDataArr.length; i++) {
+        console.log(profileDataArr[i]);
+    }
+    console.log('==========================');
+    
+    //is the same as this ...
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
