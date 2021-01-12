a33023c675db6b1e9aabe660024c235c8740764c
zanderson1998 <zanderson1998@yahoo.com>
2021-01-12 11:47:42 -0700

    modularize app.js and create an html file

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const profileDataArgs = process.argv.slice(2,process.argv.length);
-// console.log(profileDataArgs);
+//include exterior files
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = (profileDataArr) => {
-    //this ...
-    for ( let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('==========================');
-    
-    //is the same as this ...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+//establish variables
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html',generatePage(name,github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio Complete! Check out index.html to see the output!');
+});
